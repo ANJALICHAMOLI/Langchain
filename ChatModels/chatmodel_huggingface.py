4fe8ab515bcad42189d61caf7bd4534a8d9dde85
Anjali Chamoli <chamolianjali7@gmail.com>
2026-08-08 00:39:22 +0530

    hugging face chat model with api

---
+++ ChatModels/chatmodel_huggingface.py
@@ -0,0 +1,19 @@
+from langchain_huggingface import ChatHuggingFace,HuggingFaceEndpoint
+from dotenv import load_dotenv
+import os 
+
+load_dotenv()
+
+
+llm = HuggingFaceEndpoint(
+    repo_id="Qwen/Qwen2.5-7B-Instruct",
+    task="text-generation",
+    huggingfacehub_api_token=os.getenv("HUGGINGFACEHUB_API_TOKEN"),
+
+)
+
+model=ChatHuggingFace(llm=llm)
+
+
+result=model.invoke("what is the capital of India")
+print(result.content)
--- LLMS/llm_demo.py
+++ LLMS/llm_demo.py
@@ -4,7 +4,7 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-llm=ChatGoogleGenerativeAI(model="gemini-flash-latest",google_api_key=os.getenv("GEMINI_API_KEY"),temperature=0)
+llm=ChatGoogleGenerativeAI(model="gemini-flash-latest",google_api_key=os.getenv("GOOGLE_API_KEY"),temperature=0)
 
 result =llm.invoke("whats the capital of india")
-print(result.content)
+print(result)
