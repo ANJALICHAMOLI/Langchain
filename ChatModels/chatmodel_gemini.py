0edd46f6851cd22d66bbceb6d2ff2ab3789059d9
Anjali Chamoli <chamolianjali7@gmail.com>
2026-08-08 02:05:47 +0530

    embedding gemini

--- ChatModels/chatmodel_gemini.py
+++ ChatModels/chatmodel_gemini.py
@@ -1,5 +1,4 @@
 import os
-from unittest import result
 from langchain_google_genai import ChatGoogleGenerativeAI #would have been diffrent if it was open ai beacuse open ai originally had llms (text completion models) hence langchain has both openAI (LLM) and chatopenAI (chat model) classes but gemini was introduced after  chatbased models so langchain provides the chatgooglegenai class in langchain for ginimi direclty wokring with it hence using messsgaes ->chatmodels which oprates on HumanMessage, SystemMessage, AIMessage etc.therefore langchain_google_genai has only ChatGoogleGenerativeAI class and not GoogleGenerativeAI class and impicitly converts text to message only  
 from dotenv import load_dotenv
 load_dotenv()
