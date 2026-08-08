a454eedfd4f2df3c14b40c44d39f3eb1cdd20102
Anjali Chamoli <chamolianjali7@gmail.com>
2026-08-09 00:35:53 +0530

    prompt generation and stroing

--- langchain_prompts/prompt_ui.py
+++ langchain_prompts/prompt_ui.py
@@ -4,16 +4,15 @@ from dotenv import load_dotenv
 import streamlit as st
 import os 
 
-model=ChatGoogleGenerativeAI(model="gemini-flash-latest",google_api_key=os.getenv("GOOGLE_API_KEY"),temperature=0)
 
 
-from langchain_core.prompts import PromptTemplate
+from langchain_core.prompts import PromptTemplate,load_prompt
 
 load_dotenv()
 
+model=ChatGoogleGenerativeAI(model="gemini-flash-latest",google_api_key=os.getenv("GOOGLE_API_KEY"),temperature=0)
+
 st.header("Research Tool")
-
-
 
 paper_input = st.selectbox( "Select Research Paper Name", ["Attention Is All You Need", "BERT: Pre-training of Deep Bidirectional Transformers", "GPT-3: Language Models are Few-Shot Learners", "Diffusion Models Beat GANs on Image Synthesis"] )
 
@@ -21,26 +20,14 @@ style_input = st.selectbox( "Select Explanation Style", ["Beginner-Friendly", "T
 
 length_input = st.selectbox( "Select Explanation Length", ["Short (1-2 paragraphs)", "Medium (3-5 paragraphs)", "Long (detailed explanation)"] )
 
-#template
-template=PromptTemplate(template="""Please summarize the research paper titled "{paper_input}" with the following specifications:
-Explanation Style: {style_input}  
-Explanation Length: {length_input}  
-1. Mathematical Details:  
-   - Include relevant mathematical equations if present in the paper.  
-   - Explain the mathematical concepts using simple, intuitive code snippets where applicable.  
-2. Analogies:  
-   - Use relatable analogies to simplify complex ideas.  
-If certain information is not available in the paper, respond with: "Insufficient information available" instead of guessing.  
-Ensure the summary is clear, accurate, and aligned with the provided style and length.""")
+template=load_prompt('template.json')
 
-input_variables=['paper_input','style_input','length_input'],validate_template=True
-
-#fill the placeholder
-prompt=template.invoke({
-    'paper_input':paper_input,
-    'style_input':style_input,
-    'length_input':length_input
-})
-if st.button('Summarize Text'):
-    result=model.invoke(prompt)
+if st.button('Summarize The Paper'):
+    #creating a chain to invoke model ad template once
+    chain= template | model
+    result=chain.invoke({
+        'paper_input':paper_input,
+        'style_input':style_input,
+        'length_input':length_input
+    })
     st.write(result.content)
