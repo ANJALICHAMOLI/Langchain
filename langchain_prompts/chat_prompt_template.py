55b3823febda454ec07ae4e518516f66a6175f00
Anjali Chamoli <chamolianjali7@gmail.com>
2026-08-09 20:45:28 +0530

    chat prompt template

---
+++ langchain_prompts/chat_prompt_template.py
@@ -0,0 +1,12 @@
+from langchain_core.prompts import ChatPromptTemplate
+
+
+chat_template = ChatPromptTemplate.from_messages([
+    ('system', 'You are a helpful {domain} expert'),
+    ('human', 'Explain in simple terms, what is {topic}')
+])
+
+
+prompt = chat_template.invoke({'domain':'AI','topic':'layers of ai in breif'})
+
+print(prompt)
