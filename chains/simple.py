20b1f06eb2b152607816c94b10607e7db72a17e7
Anjali Chamoli <chamolianjali7@gmail.com>
2026-08-10 22:06:41 +0530

    chains

---
+++ chains/simple.py
@@ -0,0 +1,23 @@
+from langchain_groq import ChatGroq
+from dotenv import load_dotenv
+from langchain_core.prompts import PromptTemplate
+from langchain_core.output_parsers import StrOutputParser
+
+load_dotenv()
+import os
+prompt = PromptTemplate(
+    template='Generate 5 interesting facts about {topic}',
+    input_variables=['topic']
+)
+
+model = ChatGroq(model=os.getenv("GROQ_MODEL"))
+
+parser = StrOutputParser()
+
+chain = prompt | model | parser
+
+result = chain.invoke({'topic':'llm'})
+
+print(result)
+
+chain.get_graph().print_ascii()
