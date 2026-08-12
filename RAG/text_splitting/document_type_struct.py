096667ff11a40911c9beaf8a27838e8a7b92e430
Anjali Chamoli <chamolianjali7@gmail.com>
2026-08-12 22:57:37 +0530

    document tsruct based recursive splitting for speical cases like markdow or codes

---
+++ RAG/text_splitting/document_type_struct.py
@@ -0,0 +1,41 @@
+# An extension of the recursive char text splitter method designed for structured content like Python code, Markdown, or HTML, using language-specific separators.
+
+from langchain.text_splitter import RecursiveCharacterTextSplitter,Language
+
+text = """
+class Student:
+    def __init__(self, name, age, grade):
+        self.name = name
+        self.age = age
+        self.grade = grade  
+
+    def get_details(self):
+        return self.name"
+
+    def is_passing(self):
+        return self.grade >= 6.0
+
+
+# Example usage
+student1 = Student("anj", 20, 8.2)
+print(student1.get_details())
+
+if student1.is_passing():
+    print("The student is passing.")
+else:
+    print("The student is not passing.")
+
+"""
+
+# Initialize the splitter
+splitter = RecursiveCharacterTextSplitter.from_language(
+    language=Language.PYTHON,
+    chunk_size=300,
+    chunk_overlap=0,
+)
+
+# Perform the split
+chunks = splitter.split_text(text)
+
+print(len(chunks))
+print(chunks[1])
