d5feb6432f7e7fc3dc649b73d2356ec39c6b2b13
Anjali Chamoli <chamolianjali7@gmail.com>
2026-08-14 00:20:57 +0530

    components of rag

--- test.py
+++ test.py
@@ -1,14 +1,14 @@
-# import os
-# from dotenv import load_dotenv
-# import google.generativeai as genai
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
 
-# load_dotenv()
+load_dotenv()
 
-# genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
+genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
-# for model in genai.list_models():
-#     if "generateContent" in model.supported_generation_methods:
-#         print(model.name)
+for model in genai.list_models():
+    if "generateContent" in model.supported_generation_methods:
+        print(model.name)
 
 import google.generativeai as genai
 import os
@@ -21,3 +21,4 @@ genai.configure(api_key=os.getenv("GEMINI_API_KEY"))
 for model in genai.list_models():
     if "embed" in model.name.lower():
         print(model.name)
+       
