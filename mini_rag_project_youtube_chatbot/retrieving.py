953897f3cdf6d7e59fcc1a180b6c19114e606f18
Anjali Chamoli <chamolianjali7@gmail.com>
2026-08-14 19:02:40 +0530

    retriveing

---
+++ mini_rag_project_youtube_chatbot/retrieving.py
@@ -0,0 +1,24 @@
+import os 
+from dotenv import load_dotenv
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_community.vectorstores import FAISS
+load_dotenv()
+
+embedding_model = GoogleGenerativeAIEmbeddings(
+    model="models/gemini-embedding-2",
+    google_api_key=os.getenv("GOOGLE_API_KEY")
+)
+
+
+vector_store = FAISS.load_local(
+    "vectorstore",
+    embedding_model,
+    allow_dangerous_deserialization=True
+)
+
+retriver=vector_store.as_retriever(search_type='similarity',search_kwargs={"k":4})
+
+data=retriver.invoke("what is gradient boosting")
+for i,doc in enumerate(data,1):
+    print(f"\n--- Document {i} ---")
+    print(doc.page_content)
